2e43a1636805933825cfe38e3b9d2b673e679018
kevin.almada <kevoalmada@gmail.com>
2022-09-28 20:05:59 -0300

    Se suben cambios

---
+++ src/components/CartWidget/CartWirdget.jsx
@@ -0,0 +1,9 @@
+
+
+export const CartWidget = ()=>{
+  return(
+    <div>
+        <span></span>
+    </div>
+  )
+  }
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,7 @@
+export const ItemListContainer = ()=> {
+    return(
+        <div>
+            Listado
+        </div>
+    )
+}
